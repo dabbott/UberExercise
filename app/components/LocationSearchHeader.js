346ea03a7c5f154136070c552ab0b464b85d2f66
Devin Abbott <devinabbott@gmail.com>
2016-11-16 08:55:48 -0800

    Search header animation refinement

--- app/components/LocationSearchHeader.js
+++ app/components/LocationSearchHeader.js
@@ -1,8 +1,19 @@
 import React, { Component, PropTypes } from 'react'
-import { StyleSheet, Dimensions, View, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, Dimensions, View, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
 import * as Animatable from 'react-native-animatable'
 
-const transitionProps = ['top', 'left', 'height', 'width', 'shadowRadius']
+const transitionProps = {
+  hoverbar: ['top', 'left', 'height', 'width', 'shadowRadius'],
+  square: ['top', 'left'],
+  destinationBox: ['left', 'height', 'opacity'],
+  sourceBox: ['top', 'opacity'],
+  destinationText: ['top', 'left', 'fontSize', 'color'],
+  sourceText: ['top', 'opacity'],
+}
+
+const SQUARE_SIZE = 6
+
+const AnimatableTouchable = Animatable.createAnimatableComponent(TouchableWithoutFeedback)
 
 export default class LocationSearchHeader extends Component {
 
@@ -11,39 +22,142 @@ export default class LocationSearchHeader extends Component {
     onPress: () => {},
   }
 
-  render() {
-    const {expanded, onPress, children} = this.props
+  getAnimatableStyles = () => {
+    const {expanded} = this.props
     const {width: windowWidth} = Dimensions.get('window')
     const width = windowWidth - 24 * 2
 
-    const style = {
-      top: expanded ? 0 : 96,
-      left: expanded ? 0 : 24,
-      height: expanded ? 136 : 56,
-      width: expanded ? windowWidth : width,
-      shadowRadius: expanded ? 10 : 60,
+    return {
+      hoverbar: {
+        top: expanded ? 0 : 96,
+        left: expanded ? 0 : 24,
+        height: expanded ? 136 : 56,
+        width: expanded ? windowWidth : width,
+        shadowRadius: expanded ? 10 : 60,
+      },
+      square: {
+        top: expanded ? 109 : 96 + 56 / 2 - SQUARE_SIZE / 2,
+        left: expanded ? 29 : 24 + 22,
+      },
+      destinationBox: {
+        left: expanded ? 56 : 24,
+        right: 24,
+        top: 96,
+        height: expanded ? 32 : 56,
+        opacity: expanded ? 1 : 0,
+      },
+      destinationText: {
+        left: expanded ? 65 : 75,
+        top: expanded ? 103 : 112,
+        fontSize: expanded ? 15 : 20,
+        color: expanded ? '#A4A4AC' : '#525760',
+      },
+      sourceBox: {
+        left: 56,
+        right: 24,
+        height: 32,
+        top: expanded ? 56 : 96,
+        opacity: expanded ? 1 : 0,
+      },
+      sourceText: {
+        left: 65,
+        top: expanded ? 64 : 76,
+        opacity: expanded ? 1 : 0,
+      },
     }
+  }
+
+  render() {
+    const {onPress} = this.props
+    const animatableStyles = this.getAnimatableStyles()
 
     return (
-      <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
         <Animatable.View
-          style={[styles.container, style]}
-          transition={transitionProps}
+          style={[styles.square, animatableStyles.square]}
+          transition={transitionProps.square}
+        />
+        <Animatable.Text
+          style={[styles.sourceText, animatableStyles.sourceText]}
+          transition={transitionProps.sourceText}
+          pointerEvents={'none'}
         >
-          {children}
+          Work
+        </Animatable.Text>
+        <Animatable.Text
+          style={[styles.destinationText, animatableStyles.destinationText]}
+          transition={transitionProps.destinationText}
+          pointerEvents={'none'}
+        >
+          Where to?
+        </Animatable.Text>
+        <Animatable.View
+          style={[styles.destinationBox, animatableStyles.destinationBox]}
+          transition={transitionProps.destinationBox}
+          pointerEvents={'none'}
+        />
+        <Animatable.View
+          style={[styles.sourceBox, animatableStyles.sourceBox]}
+          transition={transitionProps.sourceBox}
+          pointerEvents={'none'}
+        />
+        <Animatable.View
+          style={[styles.hoverbar, animatableStyles.hoverbar]}
+          transition={transitionProps.hoverbar}
+        >
+          <TouchableOpacity
+            style={styles.target}
+            onPress={onPress}
+          />
         </Animatable.View>
-      </TouchableWithoutFeedback>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 1,
+  },
+  hoverbar: {
     position: 'absolute',
     backgroundColor: 'white',
     shadowColor: 'black',
     shadowOffset: {width: 0, height: 4},
     shadowOpacity: 0.3,
     zIndex: 1,
+  },
+  target: {
+    flex: 1,
+  },
+  square: {
+    position: 'absolute',
+    width: SQUARE_SIZE,
+    height: SQUARE_SIZE,
+    backgroundColor: 'black',
+    zIndex: 2,
+  },
+  destinationBox: {
+    position: 'absolute',
+    backgroundColor: '#EDEDED',
+    borderRadius: 4,
+    zIndex: 3,
+  },
+  destinationText: {
+    position: 'absolute',
+    zIndex: 4,
+    backgroundColor: 'transparent',
+  },
+  sourceText: {
+    position: 'absolute',
+    zIndex: 4,
+    color: '#525760',
+    backgroundColor: 'transparent',
+  },
+  sourceBox: {
+    position: 'absolute',
+    backgroundColor: '#F9F9F9',
+    borderRadius: 4,
+    zIndex: 3,
   },
 })
